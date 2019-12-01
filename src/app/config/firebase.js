10e8e6032178b26cd82e20ae485686a274e28459
Amarteli <54898416+Amarteli@users.noreply.github.com>
2019-12-01 19:24:45 +0200

    Add files via upload

---
+++ src/app/config/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuHVAcIbFnQbclVjYed7_CIG6esYwMvpI",
+    authDomain: "mobilecv-6c7a3.firebaseapp.com",
+    databaseURL: "https://mobilecv-6c7a3.firebaseio.com",
+    projectId: "mobilecv-6c7a3",
+    storageBucket: "mobilecv-6c7a3.appspot.com",
+    messagingSenderId: "516342861933",
+    appId: "1:516342861933:web:15d29b15f3b2fb2a766f64",
+    measurementId: "G-QZYT7W962K"
+};
+
+export default firebaseConfig;
