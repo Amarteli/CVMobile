b408eed17fbbfe765ebfaa722111a54fddc4b2b1
Amarteli <54898416+Amarteli@users.noreply.github.com>
2019-12-01 19:18:59 +0200

    Add files via upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAuHVAcIbFnQbclVjYed7_CIG6esYwMvpI',
+    authDomain: 'mobilecv-6c7a3.firebaseapp.com',
+    databaseURL: 'https://mobilecv-6c7a3.firebaseio.com',
+    projectId: 'mobilecv-6c7a3',
+    storageBucket: 'mobilecv-6c7a3.appspot.com',
+    messagingSenderId: '516342861933',
+    appId: '1:516342861933:web:15d29b15f3b2fb2a766f64',
+   measurementId: 'G-QZYT7W962K'
+  }
 };
 
 /*
